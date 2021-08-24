b068f4e83295276bac4f264fe7705440e3c79ec4
Idrees Ahmad <idrees.s.ahmad@icloud.com>
2021-08-24 12:38:00 -0600

    Moved env. vars to Heroku

--- hackpack/local_settings.py
+++ hackpack/local_settings.py
@@ -4,11 +4,6 @@ Configuration Settings
 
 ''' Uncomment to configure using the file.  
 WARNING: Be careful not to post your account credentials on GitHub.
-
-TWILIO_ACCOUNT_SID = "ACebc9c0c66174393f3031c888c67d056d" 
-TWILIO_AUTH_TOKEN = "dab325c61da285e7f18aebfdceb22fe0"
-TWILIO_APP_SID = "SD4f57d2e5553a968e28c875535080970d"
-TWILIO_CALLER_ID = "+12563306369"
 '''
 
 # Begin Heroku configuration - configured through environment variables.
