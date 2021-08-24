a59bd6b33a8c84af6e90cef898edfd6a72e2e5e2
Idrees Ahmad <idrees.s.ahmad@icloud.com>
2021-08-24 12:16:39 -0600

    Added auth tokens

--- hackpack/local_settings.py
+++ hackpack/local_settings.py
@@ -5,10 +5,10 @@ Configuration Settings
 ''' Uncomment to configure using the file.  
 WARNING: Be careful not to post your account credentials on GitHub.
 
-TWILIO_ACCOUNT_SID = "ACxxxxxxxxxxxxx" 
-TWILIO_AUTH_TOKEN = "yyyyyyyyyyyyyyyy"
+TWILIO_ACCOUNT_SID = "ACebc9c0c66174393f3031c888c67d056d" 
+TWILIO_AUTH_TOKEN = "dab325c61da285e7f18aebfdceb22fe0"
 TWILIO_APP_SID = "APzzzzzzzzz"
-TWILIO_CALLER_ID = "+17778889999"
+TWILIO_CALLER_ID = "+12563306369"
 '''
 
 # Begin Heroku configuration - configured through environment variables.
