1de974e80906afa75f53fb7c9e47cd029b95271e
Idrees Ahmad <idrees.s.ahmad@icloud.com>
2021-08-24 12:31:23 -0600

    Added SIP Trunk SID

--- hackpack/local_settings.py
+++ hackpack/local_settings.py
@@ -7,7 +7,7 @@ WARNING: Be careful not to post your account credentials on GitHub.
 
 TWILIO_ACCOUNT_SID = "ACebc9c0c66174393f3031c888c67d056d" 
 TWILIO_AUTH_TOKEN = "dab325c61da285e7f18aebfdceb22fe0"
-TWILIO_APP_SID = "APzzzzzzzzz"
+TWILIO_APP_SID = "SD4f57d2e5553a968e28c875535080970d"
 TWILIO_CALLER_ID = "+12563306369"
 '''
 
